347c327d7189927ccdc710a19f67e9658b2a30cb
Ellen <102437613+dangernooodle@users.noreply.github.com>
2023-05-04 19:58:16 +0200

    changes to connectionString Main class

--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -5,6 +5,10 @@ public class KeyHandler {
     private String connectionString;
     private final String keyFilePath = System.getProperty("user.home") + "/Documents/Java code stuff/API keys/shop.txt";
 
+    public KeyHandler(String connString) {
+
+    }
+
     public String getConnectionString() {
         return connectionString;
     }
@@ -27,5 +31,17 @@ public class KeyHandler {
             return;
         }
         setConnectionString(prop.getProperty("connectionString"));
+    }
+
+    public String GetKey(String string) {
+        Properties prop = new Properties();
+        try {
+            FileInputStream input = new FileInputStream(keyFilePath);
+            prop.load(input);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        return prop.getProperty(string);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,28 @@ public class Main {
 
         Logger.getLogger("org.mongodb.driver").setLevel(Level.SEVERE); // ger endast felmeddelanden av typen severe
 
-        MongoDBPersonFacade dbFacade = new MongoDBPersonFacade(); // create new instance of MongoDBPersonFacade
+        /*MongoDBPersonFacade dbFacade = new MongoDBPersonFacade(); // create new instance of MongoDBPersonFacade
         Person person = new Person("John Doe", "42", "Somewhere"); // add person
-        dbFacade.insertOne(person);
+        dbFacade.insertOne(person);*/
 
-        KeyHandler keyHandler = new KeyHandler();
-        MongoClient client = MongoClients.create(keyHandler.getConnectionString());
-        MongoDatabase db = client.getDatabase("shop");
-        MongoCollection<Document> collection = db.getCollection("customers");
+        /*KeyHandler key = new KeyHandler("MongoDB");
+        MongoDBPersonFacade db = new MongoDBPersonFacade(
+                key.GetKey("connectionString"),
+                key.GetKey("collection"),
+                key.GetKey("database"));*/
+
+        KeyHandler key = new KeyHandler("MongoDB");
+        String connectionString = key.GetKey("connectionString");
+        String collectionName = key.GetKey("collection");
+        String databaseName = key.GetKey("database");
+        if (connectionString == null || connectionString.isEmpty()) {
+            throw new IllegalArgumentException("Connection string is null or empty");
+        }
+        MongoClient client = MongoClients.create(connectionString);
+
+        //MongoClient client = MongoClients.create(key.getConnectionString());
+        MongoDatabase mongodb = client.getDatabase("shop");
+        MongoCollection<Document> collection = mongodb.getCollection("customers");
 
         FindIterable<Document> result = collection.find();
 
